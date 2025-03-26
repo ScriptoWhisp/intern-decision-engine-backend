d4c4375a96b868e2a66eacaa259cfc3c35856aa2
datjul <datjul@taltech.ee>
2025-03-26 11:30:41 +0200

    Constraints and JavaDocs fix

--- src/main/java/ee/taltech/inbankbackend/service/DecisionEngine.java
+++ src/main/java/ee/taltech/inbankbackend/service/DecisionEngine.java
@@ -23,8 +23,8 @@ public class DecisionEngine {
     /**
      * Calculates the maximum loan amount and period for the customer based on their ID code,
      * the requested loan amount and the loan period.
-     * The loan period must be between 12 and 60 months (inclusive).
-     * The loan amount must be between 2000 and 10000€ months (inclusive).
+     * The loan period must be between 12 and 48 months (inclusive).
+     * The loan amount must be between 2000 and 10000 euros (inclusive).
      *
      * @param personalCode ID code of the customer that made the request.
      * @param loanAmount Requested loan amount
